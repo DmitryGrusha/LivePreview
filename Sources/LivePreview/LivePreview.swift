1fd78ffecdc2fcf1fc94dce5be84ca4b770b973e
Dmitry Grusha <Dima-fx@ukr.net>
2023-04-10 23:39:03 +0300

    project

---
+++ Sources/LivePreview/LivePreview.swift
@@ -0,0 +1,42 @@
+import SwiftUI
+
+@available(iOS 15.0, *)
+public extension UIViewController {
+  
+  private struct LivePreview: UIViewControllerRepresentable {
+    let viewController: UIViewController
+    func makeUIViewController(context: Context) -> some UIViewController { viewController }
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) { }
+  }
+
+  func livePreview() -> some View {
+    LivePreview(viewController: self)
+  }
+}
+
+@available(iOS 15.0, *)
+public extension UIView {
+  
+  private struct LivePreview: UIViewRepresentable {
+    let view: UIView
+    func makeUIView(context: Context) -> some UIView { view }
+    func updateUIView(_ uiView: UIViewType, context: Context) { }
+  }
+  
+  func livePreview() -> some View {
+    LivePreview(view: self)
+  }
+}
+
+///Example:
+///#if DEBUG && canImport(SwiftUI)
+///import SwiftUI
+///@available(iOS 15.0, *)
+///struct ViewController_Preview: PreviewProvider {
+///  static var previews: some View {
+///    ViewController()
+///      .livePreview()
+///     .ignoresSafeArea()
+///  }
+///}
+///#endif
---
+++ Tests/LivePreviewTests/LivePreviewTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import LivePreview
+
+final class LivePreviewTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+//        XCTAssertEqual(LivePreview().text, "Hello, World!")
+    }
+}
